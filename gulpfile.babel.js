67798abe1268fc5dff76df245bb5caff993c3bca
ChangJoo Park <pcjpcj2@gmail.com>
2017-01-24 21:39:06 +0900

    SCSS-Lint 추가

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,31 +6,37 @@ import concat from 'gulp-concat'
 import connect from 'gulp-connect'
 import webpack from 'gulp-webpack'
 import eslint from 'gulp-eslint'
+import scsslint from 'gulp-scss-lint'
 import paths from './config/paths'
 import webpackConfig from './webpack.config.babel'
 
-gulp.task('sass', () => {
+gulp.task('sass', ['scsslint'], () => {
   return gulp.src(paths.sass.src)
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(gulp.dest(paths.sass.dest))
     .pipe(concat('grap-ui.css'))
     .pipe(gulp.dest('dist/css'))
-    .pipe(gulp.dest('docs/css`'))
+    .pipe(gulp.dest('docs/css'))
+})
+
+gulp.task('scsslint', () => {
+  return gulp.src('./scss/*.scss')
+    .pipe(scsslint())
 })
 
 gulp.task('eslint', () => {
   return gulp.src(['**/*.js', '!node_modules/**', '!docs/**', '!build/**', '!dist/**'])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
 })
 
 gulp.task('webpack', ['eslint'], () => {
   return gulp.src('./js/main.js')
-           .pipe(webpack(webpackConfig))
-           .pipe(concat('grap-ui.js'))
-           .pipe(gulp.dest('dist/js'))
-           .pipe(gulp.dest('docs/js'))
+    .pipe(webpack(webpackConfig))
+    .pipe(concat('grap-ui.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('docs/js'))
 })
 
 gulp.task('connect', () => {
@@ -42,17 +48,17 @@ gulp.task('connect', () => {
 
 gulp.task('html', () => {
   gulp.src('./docs/index.html')
-  .pipe(connect.reload())
+    .pipe(connect.reload())
 })
 
 gulp.task('js', () => {
   gulp.src('./js/**.js')
-  .pipe(connect.reload())
+    .pipe(connect.reload())
 })
 
 gulp.task('css', () => {
   gulp.src('./scss/**.scss')
-  .pipe(connect.reload())
+    .pipe(connect.reload())
 })
 
 gulp.task('watch', () => {
